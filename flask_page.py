7db9c3a71893dec3bc3b81d1a68f7963cc3f97df
eddie <eddie.eby@gmail.com>
2023-09-07 09:29:13 -0600

    added bootstrap and base html template

--- flask_page.py
+++ flask_page.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 # url_for and redirect redirect users to other webpages
 
 app = Flask(__name__)
@@ -6,18 +6,7 @@ app = Flask(__name__)
 # @app.route('/') is what the user will navigate to using a URL
 @app.route('/')
 def home():
-    return 'Hello! this is the main page <h1>HELLO<h1>'
-
-# @app.route('/<name>') is a way to pass a variable from the URL to the function
-@app.route('/<name>')
-def user(name):
-    return f'Hello {name}'
-
-# this is a redirect
-# you have to put the function name in url_for not the route
-@app.route('/admin')
-def admin():
-    return redirect(url_for('home'))
+    return render_template('index.html')
 
 # this is what actually starts the server
 if __name__ == '__main__':
