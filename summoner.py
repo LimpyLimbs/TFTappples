bc84ed4bb3c64fbd435a1115d91ab73bed142bce
eddie <eddie.eby@gmail.com>
2023-09-11 22:29:38 -0600

    final commit for the day

--- summoner.py
+++ summoner.py
@@ -15,7 +15,7 @@ class Summoner:
         api_key_value=os.environ.get('RIOT_API_KEY')
         api_key_header={"X-Riot-Token": api_key_value}
         
-        # API call which is cast into a list
+        # API call for a list of match IDs
         match_list=requests.get(f'https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/{self.puuid}/ids', headers=api_key_header).json()
         
         match_history=[]
